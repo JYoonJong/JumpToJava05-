7b75ab6cb9c0afc985face1166d6d93ba0b67360
JYoonJong <dbswhd493@naver.com>
2022-01-09 00:20:05 +0900

    점프 투 자바 Method 입력과 출력이 모두 없는 메소드

---
+++ src/Method4.java
@@ -0,0 +1,16 @@
+// 입력과 출력이 모두 없는 메소드
+
+
+public class Method4 {
+
+	public void say() {
+		System.out.println("Hi");
+	}
+	
+	public static void main(String[] args) {
+		Method4 method4 = new Method4();
+		method4.say();
+
+	}
+
+}
