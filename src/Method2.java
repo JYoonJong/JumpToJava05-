9092e68b6f13af690f6a7e13da077b566d0197bb
JYoonJong <dbswhd493@naver.com>
2022-01-09 00:19:13 +0900

    점프 투 자바 Method 입력은 없고 출력은 있는 메소드

---
+++ src/Method2.java
@@ -0,0 +1,19 @@
+// 입력은 없고 출력은 있는 메소드
+
+
+public class Method2 {
+	
+	public String say() {
+		return "Hi";
+	}
+
+	public static void main(String[] args) {
+		
+		Method2 method2 = new Method2();
+		String a = method2.say();
+		
+		System.out.println(a);
+
+	}
+
+}
